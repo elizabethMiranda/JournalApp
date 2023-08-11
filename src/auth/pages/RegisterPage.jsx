737490a86453661b209d548d3eb34d40352d6192
elizabethMiranda <mirandaeli23@gmail.com>
2023-08-10 21:39:52 -0600

    diseño de joURNAL

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,6 +1,45 @@
+import { Link as RouterLink } from 'react-router-dom';
+import { Grid, TextField, Typography, Button, Link } from '@mui/material';
+import { AuthLayout } from '../layout/AuthLayout';
+
 
 export const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
+    <AuthLayout title="Crear Cuenta">
+    <form>
+        <Grid container>
+            <Grid item xs={ 12 } sx={{ mt: 2 }}>
+                <TextField 
+                  label='Nombre Completo' 
+                  type='text' 
+                  placeholder='Nombre Completo'
+                  fullWidth
+                />
+            </Grid>
+            <Grid item xs={ 12 } sx={{ mt: 2 }}>
+                <TextField 
+                  label='Correo' 
+                  type='text' 
+                  placeholder='Correo electrónico'
+                  fullWidth
+                />
+            </Grid>
+            <Grid item xs={ 12 } sx={{ mt: 2 }} >
+                <TextField 
+                  label='Contraseña' 
+                  type='password' 
+                  placeholder='contraseña' 
+                  fullWidth
+                />
+            </Grid>
+            <Grid container direction='row' justifyContent='end'>
+            <Typography sx={{ mr: 1}}>¿Ya tienes cuenta?</Typography>
+              <Link component={ RouterLink } color='inherit' to='/auth/login'>
+                  Ingresar
+              </Link>
+            </Grid>
+        </Grid>
+    </form>
+    </AuthLayout>
   )
 }
